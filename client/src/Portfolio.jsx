5e592cdd89ee87614066fc3bdeee2038113acbbd
RomanEmmons <roman.emmons@gmail.com>
2020-04-14 01:01:30 -0700

    front end complete

--- client/src/Portfolio.jsx
+++ client/src/Portfolio.jsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
 
-const Portfolio = (props) => {
-  return <div>{props.page === 'portfolio' ? <p>Portfolio</p> : null}</div>;
-};
+class Portfolio extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.setState({ applications: this.props.applications });
+  }
+
+  render() {
+    return (
+      <div className="portOuter">
+        {this.props.page === 'portfolio' ? (
+          <Carousel
+            className="projects"
+            interval={20000}
+            infiniteLoop={true}
+            autoPlay={true}
+            showThumbs={false}
+          >
+            {this.state.applications
+              ? this.state.applications.map((application, i) => (
+                  <div key={i} className="portfolio">
+                    <h2>{application.title}</h2>
+                    <div className="innerPort">
+                      <div className="portfolioImage">
+                        <img src={application.image.default}></img>
+                      </div>
+                      <div className="facts">
+                        <p>{application.description}</p>
+                        <br></br>
+                        <p>Stack: {application.stack}</p>
+                        <br></br>
+                        <a href={application.github}>{application.github}</a>
+                      </div>
+                    </div>
+                  </div>
+                ))
+              : null}
+          </Carousel>
+        ) : null}
+      </div>
+    );
+  }
+}
 
 export default Portfolio;
--- server/index.js
+++ server/index.js
@@ -1,12 +1,65 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const router = express.Router();
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const creds = require('../config');
 
 const port = 3000;
 
+const transport = {
+  host: 'smtp.gmail.com', // Don’t forget to replace with the SMTP host of your provider
+  port: 587,
+  auth: {
+    user: creds.USER,
+    pass: creds.PASS,
+  },
+};
+
+var transporter = nodemailer.createTransport(transport);
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Server is ready to take messages');
+  }
+});
+
+router.post('/send', (req, res, next) => {
+  console.log('req.body', req.body);
+  var name = req.body.name;
+  var email = req.body.email;
+  var message = req.body.message;
+  var content = `name: ${name} \n email: ${email} \n message: ${message} `;
+
+  var mail = {
+    from: name,
+    to: 'roman.emmons@gmail.com',
+    subject: 'New Message from Contact Form',
+    text: content,
+  };
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: 'fail',
+      });
+    } else {
+      res.json({
+        status: 'success',
+      });
+    }
+  });
+});
+
+app.use(cors());
 app.use(bodyParser.json());
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('client/public'));
+app.use('/', router);
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
