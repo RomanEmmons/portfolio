6203341b4b45956ad8d155e23c2d12b268d9c3af
RomanEmmons <roman.emmons@gmail.com>
2020-04-14 21:44:08 -0700

    little bit of clean up

--- webpack.config.js
+++ webpack.config.js
@@ -25,18 +25,3 @@ module.exports = {
     path: __dirname + '/client/public',
   },
 };
-
-// module: {
-//   rules: [
-//     {
-//       test: /\.jsx$/,
-//       exclude: /node_modules/,
-//       use: {
-//         loader: 'babel-loader',
-//         options: {
-//           presets: ['@babel/preset-react', '@babel/preset-env'],
-//         },
-//       },
-//     },
-//   ],
-// },
