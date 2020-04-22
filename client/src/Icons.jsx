a0b58f6e4d1fe2b366d91e0ae94dc515317bb76c
RomanEmmons <roman.emmons@gmail.com>
2020-04-21 23:47:51 -0700

    added Repo Retriever

---
+++ client/src/Icons.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Icons = () => {
+  return (
+    <div className="icon-container">
+      <a href="https://github.com/RomanEmmons" className="icons2">
+        Z
+      </a>
+      <br></br>
+      <a
+        href="https://www.linkedin.com/in/roman-emmons-a4a048180"
+        className="icons1"
+      >
+        I
+      </a>
+      <br></br>
+      <a href="https://www.instagram.com/liquor_rich/?hl=en" className="icons2">
+        I
+      </a>
+      <br></br>
+      <a href="https://www.facebook.com/romandanielemmons" className="icons1">
+        F
+      </a>
+      <br></br>
+      <a href="https://twitter.com/EmmonsRoman" className="icons1">
+        L
+      </a>
+      <br></br>
+      <a href="mailto:roman.emmons@gmail.com" className="icons2">
+        M
+      </a>
+    </div>
+  );
+};
+
+export default Icons;
