924bdbae4af9334cdedcde17bd813fae4c275bd0
RomanEmmons <roman.emmons@gmail.com>
2020-04-23 00:04:34 -0700

    added blue daisy

--- client/src/Portfolio.jsx
+++ client/src/Portfolio.jsx
@@ -1,48 +1,44 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
+import {
+  chonky,
+  rsvp,
+  stock,
+  blue,
+  repo,
+} from '../../portfolioData/portfolio.js';
 
-class Portfolio extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const repos = [chonky, rsvp, stock, blue, repo];
 
-  componentDidMount() {
-    this.setState({ applications: this.props.applications });
-  }
-
-  render() {
-    return (
-      <div className="portOuter">
-        <Carousel
-          className="projects"
-          interval={20000}
-          infiniteLoop={true}
-          autoPlay={true}
-          showThumbs={false}
-        >
-          {this.state.applications
-            ? this.state.applications.map((application, i) => (
-                <div key={i} className="portfolio">
-                  <h2>{application.title}</h2>
-                  <div className="innerPort">
-                    <div className="portfolioImage">
-                      <img src={application.image.default}></img>
-                    </div>
-                    <div className="facts">
-                      <p>{application.description}</p>
-                      <p>Stack: {application.stack}</p>
-                      <br></br>
-                      <a href={application.github}>{application.github}</a>
-                    </div>
-                  </div>
-                </div>
-              ))
-            : null}
-        </Carousel>
-      </div>
-    );
-  }
-}
+const Portfolio = (props) => {
+  return (
+    <div className="portOuter">
+      <Carousel
+        className="projects"
+        interval={20000}
+        infiniteLoop={true}
+        autoPlay={true}
+        showThumbs={false}
+      >
+        {repos.map((application, i) => (
+          <div key={i} className="portfolio">
+            <h2>{application.title}</h2>
+            <div className="innerPort">
+              <div className="portfolioImage">
+                <img src={application.image.default}></img>
+              </div>
+              <div className="facts">
+                <p>{application.description}</p>
+                <p>Stack: {application.stack}</p>
+                <br></br>
+                <a href={application.github}>{application.github}</a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
 
 export default Portfolio;
