5135feae14b62eb0cbf9349e96cd68536056e522
RomanEmmons <roman.emmons@gmail.com>
2020-05-04 21:20:38 -0700

    fully responsive

--- client/src/IconsMobile.jsx
+++ client/src/IconsMobile.jsx
@@ -2,32 +2,37 @@ import React from 'react';
 
 const IconsMobile = () => {
   return (
-    <div className="icon-container hideFullSize">
-      <div>
-        <a href="https://github.com/RomanEmmons" className="icons2">
-          Z
-        </a>
-        <a
-          href="https://www.linkedin.com/in/roman-emmons-a4a048180"
-          className="icons1"
-        >
-          I
-        </a>
-        <a
-          href="https://www.instagram.com/liquor_rich/?hl=en"
-          className="icons2"
-        >
-          I
-        </a>
-        <a href="https://www.facebook.com/romandanielemmons" className="icons1">
-          F
-        </a>
-        <a href="https://twitter.com/EmmonsRoman" className="icons1">
-          L
-        </a>
-        <a href="mailto:roman.emmons@gmail.com" className="icons2">
-          M
-        </a>
+    <div className="iconsOuter">
+      <div className="icon-container hideFullSize">
+        <div>
+          <a href="https://github.com/RomanEmmons" className="icons2">
+            Z
+          </a>
+          <a
+            href="https://www.linkedin.com/in/roman-emmons-a4a048180"
+            className="icons1"
+          >
+            I
+          </a>
+          <a
+            href="https://www.instagram.com/liquor_rich/?hl=en"
+            className="icons2"
+          >
+            I
+          </a>
+          <a
+            href="https://www.facebook.com/romandanielemmons"
+            className="icons1"
+          >
+            F
+          </a>
+          <a href="https://twitter.com/EmmonsRoman" className="icons1">
+            L
+          </a>
+          <a href="mailto:roman.emmons@gmail.com" className="icons2 lastIcon">
+            M
+          </a>
+        </div>
       </div>
     </div>
   );
