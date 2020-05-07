988f1856a37f563e277f1de77d6c692377709914
RomanEmmons <roman.emmons@gmail.com>
2020-05-06 17:41:59 -0700

    added ecosystem.config.js file

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: 'porfolio',
+      cwd: './server',
+      script: 'npm',
+      args: 'start',
+      autorestart: true,
+      watch: true,
+      max_memory_restart: '1G',
+      // env : {
+      //   USER: 'roman.emmons@gmail.com',
+      //   PASS: ''
+      // }
+    },
+  ],
+};
